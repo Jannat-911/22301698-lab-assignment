e398682d6cceb0281203866c3843ddefd4505924
Jannat-911 <151717233+Jannat-911@users.noreply.github.com>
2026-05-08 21:49:55 +0600

    Update setticket.c

--- testscripts/setticket.c
+++ testscripts/setticket.c
@@ -2,7 +2,6 @@
 #include "stat.h"
 #include "user.h"
 #include "pstat.h"
-
 #define check(exp, msg) if(exp) {} else {\
    printf(1, "%s:%d check (" #exp ") failed: %s\n", __FILE__, __LINE__, msg);\
    exit();}
